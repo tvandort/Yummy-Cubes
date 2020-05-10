8df024f31c59fee9cf68f06f0ac1a91a24c85c3e
Tom <tvandort1@gmail.com>
2020-05-10 18:47:48 -0400

    Add some next stuff?

--- app/index.ts
+++ app/index.ts
@@ -1,1 +1,24 @@
-console.log("what??");
+import express from "express";
+import { Server } from "http";
+import socketio from "socket.io";
+import { resolve } from "path";
+
+const app = express();
+const server = new Server(app);
+const io = socketio(server);
+
+app.get("/", (req, res) => {
+  res.sendFile(resolve("./app/index.html"));
+});
+
+io.on("connection", (socket) => {
+  console.log("A user connected?");
+
+  socket.on("message", (message) => {
+    console.log(message);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("listening on port 3000");
+});
