07e7de699d5ed47a00bb7434ee627d49a1e68c2a
Tom <tvandort1@gmail.com>
2020-05-11 10:55:50 -0400

    Remove home route.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,17 +1,12 @@
 import express from "express";
 import { Server } from "http";
 import socketio from "socket.io";
-import { resolve } from "path";
 import { config } from "dotenv";
 
 config();
 const app = express();
 const server = new Server(app);
 const io = socketio(server);
-
-app.get("/", (req, res) => {
-  res.sendFile(resolve("./public/api-status.html"));
-});
 
 io.on("connection", (socket) => {
   console.log("user joined");
