c2017f56ffb225a3fdc2f9e5840bc34b3de705e7
Tom <tvandort1@gmail.com>
2020-05-16 10:31:14 -0400

    Add some commentary about forktschecker

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 module.exports = {
   webpack: (config, { dev }) => {
     if (dev) {
+      // Add eslint to webpack. Fail on warning and on error so that the
+      // error overlay shows up on dev.
       config.module.rules.push({
         test: /\.((t|j)sx?)$/,
         loader: "eslint-loader",
@@ -11,6 +13,10 @@ module.exports = {
         options: { failOnWarning: true, failOnError: true }
       });
 
+      // Add a typescript error overlay. `async: false` makes the compilation
+      // slower but it allows the error overlay in the browser.
+      // If compilation speed ever becomes a real concern enable async and deal
+      // with console error messages.
       config.plugins.push(new ForkTsCheckerWebpackPlugin({ async: false }));
     }
 
