bb0751a5fbc58277dd62d1af9f1d83f5766dfb24
Tom <tvandort1@gmail.com>
2020-05-25 03:38:32 -0400

    Example of a quick in memory store.

--- src/fakedb/rooms.ts
+++ src/fakedb/rooms.ts
@@ -2,6 +2,26 @@ export interface Room {
   id: string;
 }
 
-const rooms: Room[] = [];
+export class Rooms {
+  private rooms: { [key: string]: Room };
+
+  constructor({ rooms }: { rooms: Room[] } = { rooms: [] }) {
+    const init: { [key: string]: Room } = {};
+    this.rooms = rooms.reduce((rooms, room) => {
+      rooms[room.id] = room;
+      return rooms;
+    }, init);
+  }
+
+  add(room: Room) {
+    if (room.id in rooms) {
+      throw new Error('Room already exists.');
+    }
+
+    this.rooms[room.id] = room;
+  }
+}
+
+const rooms = new Rooms();
 
 export default rooms;
